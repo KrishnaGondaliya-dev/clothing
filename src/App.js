eb09e64b9b6a2c6759b8aadb42dd3b320d245c4c
krishna gondaliya <gondaliya.krishna12@gmail.com>
2021-04-16 18:40:33 +0530

    second comment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './App.css';
-import HomePage from './homePage.component';
-
+import HomePage from './pages/homepage/homePage.component';
+import  {BrowserRouter} from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <HomePage/>
+      </BrowserRouter>
     </div>
   );
 }
